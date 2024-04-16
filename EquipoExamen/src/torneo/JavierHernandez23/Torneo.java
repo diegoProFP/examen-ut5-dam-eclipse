58f30799cb9be0dcca769acc4046cbdb136d6c83
javihergomm <javihergomgaragu@gmail.com>
2024-04-16 13:41:56 +0200

    Se ha hecho el Javadoc

--- EquipoExamen/src/torneo/JavierHernandez23/Torneo.java
+++ EquipoExamen/src/torneo/JavierHernandez23/Torneo.java
@@ -8,6 +8,13 @@ import java.util.List;
 //Poner comentario de la clase, con la descripción de qué es lo que hace  
 //De autor poned vuestro email de educaMadrid. 
 //La version de la clase es la 1.5, y existe desde la 1.0
+/**
+ * En esta clase se crean torneos, creando los partidos, comprobando si existen, etc.
+ * 
+ * @author javier.hernandez23
+ * @version 1.5
+ * @since 1.0
+ */
 public class Torneo {
 	
     private String nombre;
@@ -16,6 +23,12 @@ public class Torneo {
     // Poner descripcion, parametros de entrada, valor de retorno, y en qué condiciones se produce la excepción. Además que pueda
  	// referenciar tanto a las clases Partido y DatosInvalidosException
  	//Existe desde la version 1.0
+/**
+ * Agrega partidos comprobando que los datos de entrada sean correctos
+ * @param nuevo
+ * @return boolean
+ * @throws DatosInvalidosException
+ */
     public boolean agregarPartido(Partido nuevo) throws DatosInvalidosException {
         if (nuevo == null || nuevo.getEquipoLocal() == null || nuevo.getEquipoVisitante() == null) {
             throw new DatosInvalidosException("El partido o alguno de los equipos es nulo");
@@ -31,6 +44,15 @@ public class Torneo {
 
     // Poner descripcion, parametros de entrada, valor de retorno.
    	//Existe desde la version 1.0
+    
+    /**
+     * 
+     * 
+     * @param local
+     * @param visitante
+     * @return boolean
+     * @since 1.0
+     */
     public boolean encontrarPartidoPorEquipos(String local, String visitante) {
         for (Partido partido : partidos) {
             if (partido.getEquipoLocal().equalsIgnoreCase(local) && partido.getEquipoVisitante().equalsIgnoreCase(visitante)) {
@@ -43,6 +65,13 @@ public class Torneo {
     // Este metodo está deprecado porque se ha hecho uno mejor, y el que lo sustituye es el
    	// encontrarPartidos(Date fecha, String equipoLocal, String equipoVisitante)
    	//Existe desde la version 1.2
+    /**
+     * 
+     * @param fecha
+     * @return boolean
+     * @since 1.2
+     * @deprecated encontrarPartidos
+     */
     public boolean encontrarPartidoPorFecha(Date fecha) {
         for (Partido partido : partidos) {
             if (partido.getFechaPartido().equals(fecha)) {
@@ -54,6 +83,14 @@ public class Torneo {
 
     // Poner descripcion, parametros de entrada, valor de retorno.
  	//Existe desde la version 1.5
+    /**
+     * 
+     * @param fecha
+     * @param equipoLocal
+     * @param equipoVisitante
+     * @return lista de partidos
+     * @since 1.5
+     */
     public List<Partido> encontrarPartidos(Date fecha, String equipoLocal, String equipoVisitante) {
         List<Partido> resultados = new ArrayList<>();
         for (Partido partido : partidos) {
